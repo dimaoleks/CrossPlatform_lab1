65cb304cb9c54798fca2b60de780a27d1f67557a
Dmytro Oleksandryuk <dima.oleksandryuk@gmail.com>
2024-02-19 00:07:17 +0200

    Initial commit

---
+++ src/main/java/dimaoleks/clossplatform_lab1/ClossPlatformLab1Application.java
@@ -0,0 +1,13 @@
+package dimaoleks.clossplatform_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClossPlatformLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClossPlatformLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/dimaoleks/clossplatform_lab1/ClossPlatformLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dimaoleks.clossplatform_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClossPlatformLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
